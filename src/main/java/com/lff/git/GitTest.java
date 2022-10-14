77c67ab4d5197d3fe3ed1f4e76d5436f3394c528
TinkerBell <1187412030@qq.com>
2022-10-14 10:49:28 +0800

    hotfix commit

--- src/main/java/com/lff/git/GitTest.java
+++ src/main/java/com/lff/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println(123123);
+        System.out.println("hello hot-fix");
     }
 }
